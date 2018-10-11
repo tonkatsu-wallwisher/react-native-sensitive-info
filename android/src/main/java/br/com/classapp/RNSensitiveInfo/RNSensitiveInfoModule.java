b133127bf5f01b89611c015e2b565f8c83887365
Pavel Bakharev <baharevpavel@gmail.com>
2018-10-11 10:55:03 +0300

    Fixed Android 8.0 inability to retrive secretKey from the keystore

--- android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoModule.java
+++ android/src/main/java/br/com/classapp/RNSensitiveInfo/RNSensitiveInfoModule.java
@@ -309,7 +309,7 @@ public class RNSensitiveInfoModule extends ReactContextBaseJavaModule {
         try {
             if (!mKeyStore.containsAlias(KEY_ALIAS)) {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_RSA, ANDROID_KEYSTORE_PROVIDER);
+                    KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, ANDROID_KEYSTORE_PROVIDER);
                     keyGenerator.init(
                             new KeyGenParameterSpec.Builder(KEY_ALIAS,
                                     KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
@@ -590,8 +590,8 @@ public class RNSensitiveInfoModule extends ReactContextBaseJavaModule {
     
     public String encrypt(String input) throws Exception {
         byte[] bytes = input.getBytes();
-
         Cipher c;
+        
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Key secretKey = ((KeyStore.SecretKeyEntry) mKeyStore.getEntry(KEY_ALIAS, null)).getSecretKey();
             c = Cipher.getInstance(AES_GCM);
